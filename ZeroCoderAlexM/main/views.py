058e913c988f67e02840d8b67156153cd255c56e
Alex <guardamar81@gmail.com>
2024-08-05 22:30:08 +0400

    7
    7

--- ZeroCoderAlexM/main/views.py
+++ ZeroCoderAlexM/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'caption': "CatDjango"
+    }
+    return render(request, 'main/index.html', data)
 def new(request):
     return render(request, 'main/new.html')
