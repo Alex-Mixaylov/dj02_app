8faace239fc6d2b75bf169b74c6991a9061493d2
Alex <guardamar81@gmail.com>
2024-08-09 22:10:38 +0400

    OK
    OK

--- ZeroCoderAlexM/main/urls.py
+++ ZeroCoderAlexM/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('page1', views.page1, name='page1'),
     path('page2', views.page2, name='page2'),
+    path('page3', views.page3, name='page3')
 ]
--- ZeroCoderAlexM/main/views.py
+++ ZeroCoderAlexM/main/views.py
@@ -9,3 +9,6 @@ def page1(request):
     return render(request, 'main/page1.html')
 def page2(request):
     return render(request, 'main/page2.html')
+
+def page3(request):
+    return render(request, 'main/page3.html')
